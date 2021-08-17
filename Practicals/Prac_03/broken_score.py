90965b9f8dd9c2f30caf213ccc909d2e3e73909a
Kazuki <kazukipickersgill@GMail.com>
2021-08-17 13:41:03 +1000

    Refactor broken score program to use functions

--- Practicals/Prac_03/broken_score.py
+++ Practicals/Prac_03/broken_score.py
@@ -2,14 +2,37 @@
 Kazuki Pickersgill
 CP1404 "Broken Score"
 """
+import random
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = get_score()
+    result = get_result(score)
+    print(result)
+    random_score = get_random_score()
+    result = get_result(random_score)
+    print(result)
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_random_score():
+    random_number = random.randint(0, 100)
+    return random_number
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
