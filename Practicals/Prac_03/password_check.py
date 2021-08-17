604bd996e4a548c63610902d3facc435e492a64f
Kazuki <kazukipickersgill@GMail.com>
2021-08-17 13:16:16 +1000

    Refactor password checker program to use functions

--- Practicals/Prac_03/password_check.py
+++ Practicals/Prac_03/password_check.py
@@ -5,9 +5,24 @@ Password checker handwritten code
 """
 
 MIN_LENGTH = 5
-password = input("Type in password: ")
-while len(password) < MIN_LENGTH:
-    print("Password to short", {}).format(MIN_LENGTH)
+
+
+def main():
+    password = get_password()
+    while len(password) < MIN_LENGTH:
+        print("Password to short, {} is the minimum password length".format(MIN_LENGTH))
+        password = get_password()
+    print_asterick_password(password)
+
+
+def print_asterick_password(password):
+    for characters in password:
+        print("*", end="")
+
+
+def get_password():
     password = input("Type in password: ")
-for characters in password:
-    print("*", end="")
+    return password
+
+
+main()
