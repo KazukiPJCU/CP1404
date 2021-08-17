f9b8389ca52a9b019c564e12325a1ee29059c44c
Kazuki <kazukipickersgill@GMail.com>
2021-08-17 13:28:05 +1000

    Refactor temperatures program to use functions

---
+++ Practicals/Prac_03/broken_score.py
@@ -0,0 +1,15 @@
+"""
+Kazuki Pickersgill
+CP1404 "Broken Score"
+"""
+
+
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
