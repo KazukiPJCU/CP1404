49808a853d290624438d82ddfd70bd5bfbddb701
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2021-09-02 07:21:23 +1000

    Add word occurrences code that splits text, counting the frequency of each word that occurs before the split words are turned into list to be sorted

--- Practicals/prac_05/word_occurrences.py
+++ Practicals/prac_05/word_occurrences.py
@@ -2,3 +2,17 @@
 CP1404/CP5632 Practical
 Word occurrences
 """
+word_to_count = {}
+# example_text = "This is a collection of words of nice words this is a fun thing it is"
+text = input("Text: ")
+words = text.split()
+for word in words:
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
+
+words = list(word_to_count.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_to_count[word]))
