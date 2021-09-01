330364f5eafba80f5fdaf67dd1e41e03ce055459
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2021-09-02 06:59:21 +1000

    Add dict of 10 Hexadeximal colours

---
+++ Practicals/prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical
+Hexadecimal colour lookup
+"""
+
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff"}
+
