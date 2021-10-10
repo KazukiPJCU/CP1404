a9fd623d5895d1cc88bbed4eaedcd7422e47859c
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2021-10-10 20:16:25 +1000

    Add import classes into taxi simulator

---
+++ Practicals/prac_08/taxi_simulator.py
@@ -0,0 +1,12 @@
+"""
+CP1404
+Kazuki Pickersgill
+prac_08
+Taxi Simulator
+"""
+
+
+from Practicals.prac_06.car import Car
+from Practicals.prac_08.taxi import Taxi
+from Practicals.prac_08.silver_service_taxi import SilverServiceTaxi
+
