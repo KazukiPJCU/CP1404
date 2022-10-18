4737b9b2365d3906b2472cb679b2389104c8ab01
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-10-19 06:27:18 +1000

    Add hex colours input and print statement

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -9,3 +9,7 @@ HEX_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
              "aquamarine4": "#458b74", "azure1": "#f0ffff",
              "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
              "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    print("{:<3} colour code is {}".format(colour_name, HEX_CODES.get(colour_name)))
+    colour_name = input("Enter a colour name: ")
