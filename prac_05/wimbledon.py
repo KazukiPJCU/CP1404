5576c9bec7211e2bd8b2392ae839f9865b840d3e
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-10-19 08:10:59 +1000

    finish wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -12,7 +12,30 @@ INDEX_CHAMPION = 2
 
 def main():
     records = get_records(FILENAME)
+    """print statement to check correct formatting"""
     print(records)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
+
+
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
+
+
+def display_results(champion_to_count, countries):
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print("These {}countries have won Wimbledon ".format(len(countries)))
+    print(", ".join(country for country in sorted(countries)))
 
 
 def get_records(filename):
