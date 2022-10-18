40aae8a1ca8eb33c5248f0e3e1d7878a355e9718
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-10-19 07:44:16 +1000

    Add file reading and print statement

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,5 +5,24 @@ Estimate: 90
 Actual:
 """
 
-
 FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
+
+
+def main():
+    records = get_records(FILENAME)
+    print(records)
+
+
+def get_records(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+main()
