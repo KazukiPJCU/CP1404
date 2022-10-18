73b56528eeb1215af764cc53e898efc1fd0b6fd0
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-10-19 06:47:58 +1000

    Add formatting fix to print statement

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,12 +7,13 @@ Actual:
 
 word_to_count = {}
 text = input("TEXT: ")
+example_text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 for word in words:
     word_occurrence = word_to_count.get(word, 0)
     word_to_count[word] = word_occurrence + 1
 words = list(word_to_count.keys())
 words.sort()
+max_word_length = max(len(word) for word in words)
 for word in words:
-    print(f"{word}, {word_to_count[word]}")
-    print("{:{}},{}".format(word, 10, word_to_count[word]))
+    print("{:{}}, {}".format(word, max_word_length, word_to_count[word]))
