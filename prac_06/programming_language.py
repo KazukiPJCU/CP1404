20958095fdf8d9808d8ea374c339e0d598b6f8c9
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-02 08:42:10 +1000

    Add remove test method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -17,22 +17,3 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """If dynamic"""
         return self.typing == "Dynamic"
-
-
-def run_test():
-    """Run test"""
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-
-    languages = [ruby, python, visual_basic]
-    print(python)
-
-    print("The dynamic languages are:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.name)
-
-
-if __name__ == "__main__":
-    run_test()
