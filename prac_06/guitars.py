63197a8eced073d4c0842c61ad83f49fbf2bf107
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-02 16:52:23 +1000

    Add reformatting guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,8 +6,7 @@ from prac_06.guitar import Guitar
 
 def main():
     """Guitar program"""
-    guitars = []
-
+    guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
     print("My guitars!")
     name = input("Name: ")
     while name != "":
@@ -18,9 +17,6 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-
     if guitars:
         guitars.sort()
         print("These are my guitars:")
@@ -30,7 +26,7 @@ def main():
                 vintage_string = " (vintage)"
             print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
     else:
-        print("No guitars :( Quick, go and buy one!")
+        print("No guitars")
 
 
 main()
