1015661c429884b91111487e8af3a15ac62267eb
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-03 14:40:02 +1000

    Add fix to arithmetic code

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,7 +7,7 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year, arithmetic):
+    def __init__(self, name, typing, reflection, year,arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
@@ -27,17 +27,17 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, True)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
-
-    languages = [ruby, python, visual_basic]
-    print(python)
-
-    print("The dynamically typed languages are:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.name)
+    # ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, True)
+    # python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    # visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    #
+    # languages = [ruby, python, visual_basic]
+    # print(python)
+    #
+    # print("The dynamically typed languages are:")
+    # for language in languages:
+    #     if language.is_dynamic():
+    #         print(language.name)
 
 
 if __name__ == "__main__":
