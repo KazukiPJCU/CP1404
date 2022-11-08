7de6ac04825d9ef27ce19e4ed25ea2bd9d6b680a
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-09 08:09:47 +1000

    Add string formatting for class

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,7 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: {self.cost_estimate}, " \
+               f"completion: {self.completion_percentage} "
