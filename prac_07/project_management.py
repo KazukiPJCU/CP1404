55f3de513440e322d80e8c492fe8f8866bd28a9b
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-09 08:40:43 +1000

    Add load projects method

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -16,6 +16,7 @@ def main():
     while menu_choice != "Q":
         if menu_choice == "L":
             print("Load Projects")
+            load_projects()
         elif menu_choice == "S":
             print("Save Projects")
         elif menu_choice == "D":
@@ -30,6 +31,21 @@ def main():
             print("Incorrect menu choice")
         menu_choice = input("Menu Choice: ").upper()
     print("Thanks for coming")
+
+
+def load_projects():
+    """Load projects."""
+    projects = []
+    filename = input("Type in project file to load: ")
+    with open(filename, "r") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            date_string = parts[1]
+            date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+            project = Project(parts[0], date, int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project)
+    return projects
 
 
 main()
