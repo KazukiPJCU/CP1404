9f1814825829939ccfa22408a8ca0015cef071be
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2022-11-09 08:10:50 +1000

    Add completion method

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,3 +12,6 @@ class Project:
     def __repr__(self):
         return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: {self.cost_estimate}, " \
                f"completion: {self.completion_percentage} "
+
+    def is_complete(self):
+        return self.completion_percentage == 100
